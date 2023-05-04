143e0c1a86a6e39aab50dc169649e6190f994243
xiyu <510993715@qq.com>
2023-05-04 23:20:14 +0800

    创建FiberRoot、HostRootFiber、Update对象

--- demo/src/app.js
+++ demo/src/app.js
@@ -1,10 +1,18 @@
 // import Button from "./button.js";
 
-export default function app() {
-  return (
-    <div>
-      <h1>测试react-mini</h1>
-      {/* <Button /> */}
-    </div>
-  );
-}
+// export default function app() {
+//   return (
+//     <div>
+//       <h1>测试react-mini</h1>
+//       <Button />
+//     </div>
+//   );
+// }
+
+// todo: 暂时不支持Function类型的组件，所以先直接导出jsx
+export default (
+  <div>
+    <h1>测试react-mini</h1>
+    {/* <Button /> */}
+  </div>
+);
--- demo/src/index.js
+++ demo/src/index.js
@@ -1,5 +1,6 @@
+// React必须引入，因为要使用React.createElement方法，将jsx转为ReactElement
 import React from "../../react-mini/react/index.js";
 import { createRoot } from "../../react-mini/react-dom/index.js";
 import App from "./app.js";
 
-createRoot(document.getElementById("app")).render(<App />);
+createRoot(document.getElementById("app")).render(App);
